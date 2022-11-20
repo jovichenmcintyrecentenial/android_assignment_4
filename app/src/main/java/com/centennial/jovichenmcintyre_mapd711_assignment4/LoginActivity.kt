14e71685b53cb38354924f1dfd7bbc27b98398ef
jovi848 <jovi848.mcintyre@gmail.com>
2022-11-20 13:18:25 -0500

    add dependencies

--- app/src/main/java/com/centennial/jovichenmcintyre_mapd711_assignment4/LoginActivity.kt
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_assignment4/LoginActivity.kt
@@ -3,7 +3,7 @@ package com.centennial.jovichenmcintyre_mapd711_assignment4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
