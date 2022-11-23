e999927b3878699f2c3fb49c005241c52a31fe15
jovi848 <jovi848.mcintyre@gmail.com>
2022-11-22 23:25:01 -0500

    add comments on dao

--- app/src/main/java/com/centennial/jovichenmcintyre_mapd711_assignment4/dao/PhoneStoreDao.kt
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_assignment4/dao/PhoneStoreDao.kt
@@ -10,16 +10,19 @@ import com.centennial.jovichenmcintyre_mapd711_assignment4.models.ProductOrder
 @Dao
 interface PhoneStoreDao {
 
-    //defining an insert method using @Insert Annotation
+    //insert customer data
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertCustomer(studentModel: CustomerModel)
 
+    //check use credenttials and return custom obj on match
     @Query("SELECT * FROM customers WHERE email =:userName AND password =:password")
     fun passwordCheck(userName: String,password:String) : CustomerModel?
 
+    //get customer based on their email
     @Query("SELECT * FROM customers WHERE email =:userName")
     fun getCustomer(userName: String) : CustomerModel?
 
+    //update customer model
     @Update
     fun updateCustomer(customerModel:CustomerModel)
 
@@ -31,16 +34,16 @@ interface PhoneStoreDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertOrder(orderModel: OrderModel)
 
+    //get all products
     @Query("SELECT * FROM products")
     fun getAllProducts() : List<ProductModel>?
 
+    // get order and product using join query
     @Query("SELECT orders.*, products.* FROM orders INNER JOIN products ON products.productId = orders.prodId WHERE custId =:id")
     fun getMyOrders (id:Int) : List<ProductOrder>?
 
+    //get product by phone make and model
     @Query("SELECT * FROM products WHERE phoneMake =:phoneMake AND phoneModel =:phoneModel")
     fun getProduct (phoneMake:String, phoneModel:String) : ProductModel?
-//
-//    //defining a query method using @Query Annotation
-//    @Query("SELECT * FROM student WHERE StudentName =:studentname")
-//    fun getCustomers(studentname: String?) : LiveData<CustomerModel>
+
 }
